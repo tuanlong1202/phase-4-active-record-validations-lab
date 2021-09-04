29ca41c20fbc6ce1bc25b68ca295741ad299ea84
tuanlong1202 <tuanlong1202@gmail.com>
2021-09-04 00:07:52 -0400

    Phase 4 active record validations lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ['Fiction', 'Non-Fiction']
+    
+    validate :clickbait
+
+    def clickbait
+        if !(title.to_s.include?("Won't Believe") || title.to_s.include?("Secret") || title.to_s.include?("Top [number]") || title.to_s.include?("Guess"))
+            errors.add(:title, "The title must follow the rule!")
+        end
+    end
+
 end
